7c427ef559c90e53a414c77235bad532d50a6c23
HanieSolaty <88339589+HanieSolaty@users.noreply.github.com>
2023-03-15 15:41:47 +0330

    adding weather icon API and filtering color of icons

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+let tempInCentigrade;
+
 function setApiUrl(city) {
   const apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`;
@@ -37,12 +39,22 @@ function setWeatherAtrr(response) {
   let humidity = Math.round(response.data.main.humidity);
   let wind = Math.round(response.data.wind.speed);
   let temp = Math.round(response.data.main.temp);
+  tempInCentigrade = temp;
+  document.querySelector("#centi").classList.add("active");
+  document.querySelector("#faren").classList.remove("active");
+  let iconCode = response.data.weather[0].icon;
 
   document.querySelector("#city").textContent = city;
   document.querySelector("#description").textContent = description;
   document.querySelector("#humidity").textContent = humidity;
   document.querySelector("#wind").textContent = wind;
   document.querySelector("#temp").textContent = temp;
+  document
+    .querySelector("#temp-icon")
+    .setAttribute(
+      "src",
+      `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+    );
 }
 
 function setDafault() {
@@ -62,3 +74,22 @@ function searchProcess(event) {
 
 document.querySelector("form").addEventListener("submit", searchProcess);
 document.querySelector("#searchBtn").addEventListener("click", searchProcess);
+
+function toCentigrade(e) {
+  e.preventDefault();
+  document.querySelector("#centi").classList.add("active");
+  document.querySelector("#faren").classList.remove("active");
+  document.querySelector("#temp").textContent = tempInCentigrade;
+}
+
+function toFahrenheit(e) {
+  e.preventDefault();
+  document.querySelector("#faren").classList.add("active");
+  document.querySelector("#centi").classList.remove("active");
+  document.querySelector("#temp").textContent = Math.round(
+    (tempInCentigrade * 9) / 5 + 32
+  );
+}
+
+document.querySelector("#centi").addEventListener("click", toCentigrade);
+document.querySelector("#faren").addEventListener("click", toFahrenheit);
